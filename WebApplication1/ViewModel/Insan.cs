9ef64b8c6a4b09d1452333d358e093d910f3fe1d
malkockizi <91914764+malkockizi@users.noreply.github.com>
2021-10-11 23:58:12 +0300

    Initial commit

---
+++ WebApplication1/ViewModel/Insan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.ViewModel
+{
+    public class Insan
+    {
+        public int id{get; set;}
+        public string Adi { get; set; }
+
+        public string Soyadi { get; set; }
+
+        public string Memleketi { get; set; }
+
+        public DateTime DogumTarihi { get; set; }
+
+
+    }
+}
